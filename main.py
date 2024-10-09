5c00969c791f448707c6ab76e12df00d15965955
olijan35 <janiszewskioliwier35@gmail.com>
2024-10-09 17:12:25 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Coś wyjątkowo dziwnego lub zawstydzającego",
+            "LOL": "Częsta reakcja na coś zabawnego",
+            }
+
+
+word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print('Nie ma takiego slowa ')
